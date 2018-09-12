ab7c2f5949fd493b1aa95b2d510b6fa2408abe85
Vitali Zatroutine <vitaliz@q4websystems.com>
2018-09-11 21:04:42 -0400

    Adjustment of root and inital deployment setup

--- src/containers/Root/Root.container.js
+++ src/containers/Root/Root.container.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-import HomeContainer from '../Home/Home.container';
+// import HomeContainer from '../Home/Home.container';
 import SlideShowContainer from '../SlideShow/SlideShow.container';
 
 class Root extends Component {
@@ -10,8 +10,9 @@ class Root extends Component {
             <div className='root-container'>
                 <Router>
                     <div className='root-container_inner'>
-                        <Route exact path='/' component={() => <HomeContainer/>}/>
-                        <Route exact path='/slideshow' component={() => <SlideShowContainer/>}/>
+                        {/*<Route exact path='/' component={() => <HomeContainer/>}/>*/}
+                        <Route exact path='/' component={() => <SlideShowContainer/>}/>
+                        {/*<Route exact path='/slideshow' component={() => <SlideShowContainer/>}/>*/}
                     </div>
                 </Router>
             </div>
