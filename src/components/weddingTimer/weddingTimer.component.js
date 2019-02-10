9bf9725051b013e33d73f066fd216ef2d3a7ffe7
Vitali Zatroutine <vitaliz@q4inc.com>
2019-02-10 17:42:30 -0500

    added wedding timer component

---
+++ src/components/weddingTimer/weddingTimer.component.js
@@ -0,0 +1,64 @@
+import React, {PureComponent} from 'react';
+import moment from 'moment';
+import PropTypes from 'prop-types';
+import './weddingTimer.component.css';
+
+class WeddingTimer extends PureComponent {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            daysToWedding: moment('07-09-2019', 'DD-MM-YYYY').diff(moment(), 'days')
+        };
+    }
+
+    getDays = () => {
+        const {daysToWedding} = this.state;
+        let days = [0, 0, 0];
+
+        if (!daysToWedding) {
+            return days;
+        }
+
+        const daysString = daysToWedding.toString();
+
+        switch (daysString.length) {
+            case 1:
+                return [0, 0, daysString[0]];
+            case 2:
+                return [0, daysString[0], daysString[1]];
+            case 3:
+            default:
+                return [daysString[0], daysString[1], daysString[2]];
+
+        }
+    };
+
+    render() {
+        const days = this.getDays();
+
+        if (!days || !days.length) {
+            return null;
+        }
+
+        return (
+            <section className='wedding-timer'>
+                <div className='wedding-timer_inner'>
+                    <div className='wedding-timer_day-list'>
+                        {days.map((dayBlock) => <li className='wedding-timer_day-block'>{dayBlock}</li>)}
+                    </div>
+                    <div className='wedding-timer_message'>
+                        <span className='wedding-timer_text'>days until</span>
+                        <span className='wedding-timer_text wedding-timer_text--large'>I do</span>
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
+
+WeddingTimer.propTypes = {
+};
+
+export default WeddingTimer;
--- src/containers/Home/Home.container.js
+++ src/containers/Home/Home.container.js
@@ -3,8 +3,9 @@ import {loadPhotoset} from '../../utils/photo.util';
 import HeroBanner from '../../components/heroBanner/heroBanner.component';
 import Lights from '../../components/lights/lights.component';
 import Stars from '../../components/stars/stars.component';
-import Spinner from "../../components/spinner/spinner.component";
-import RSVP from "../../components/rsvp/modal/rsvpModal.component";
+import Spinner from '../../components/spinner/spinner.component';
+import RSVP from '../../components/rsvp/modal/rsvpModal.component';
+import WeddingTimer from '../../components/weddingTimer/weddingTimer.component';
 
 class HomeContainer extends Component {
 
@@ -60,7 +61,7 @@ class HomeContainer extends Component {
                     <Stars count={isRSVPModalOpen ? 0 : 30}/>
                 </HeroBanner>
 
-
+                <WeddingTimer/>
 
                 <RSVP
                     onModalOpen={this.handleRSVPModalOpen}
